0924a977552cf465b5f119c88f872f950abeffd6
morandave <100688146+morandave@users.noreply.github.com>
2024-05-14 00:05:49 +0800

    Update site.config.ts

--- site.config.ts
+++ site.config.ts
@@ -55,11 +55,11 @@ export default siteConfig({
   navigationLinks: [
     {
       title: 'About',
-      pageId: 'About-ecc004c919ec49fb8472199ccb45578f?pvs=74'
+      pageId: 'About-ecc004c919ec49fb8472199ccb45578f?pvs=4'
     },
     {
       title: 'Contact',
-      pageId: 'Contact-0f3b707a13d24dc7a255302b910cb2d4?pvs=74'
+      pageId: 'Contact-0f3b707a13d24dc7a255302b910cb2d4?pvs=4'
     }
   ]
 })
